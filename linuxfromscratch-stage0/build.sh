3f0421fb0a2bca9c46f3c9cb1f070dcd73308610
Craig Condit <ccondit@randomcoder.org>
2015-09-21 18:28:37 -0500

    Added build scripts

---
+++ linuxfromscratch-stage0/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname $0)"
+cd "$(pwd -P)"
+
+echo "Building stage0..."
+time docker build -t ccondit/linuxfromscratch-stage0 .
+
+echo "Starting temporary container for export..."
+docker run --name=linuxfromscratch-stage0-tmp ccondit/linuxfromscratch-stage0
+
+echo "Exporting filesystem..."
+docker export linuxfromscratch-stage0-tmp | gzip -c > linuxfromscratch-stage0.tar.gz
+
+echo "Stage0 generated."
