ea55497514ed463d4a3f3b53071f8f336b028204
Craig Condit <ccondit@randomcoder.org>
2015-09-21 19:47:40 -0500

    Make stage0 build create sqaushed repo

--- build-stage0.sh
+++ build-stage0.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd "$(dirname $0)"
-cd "$(pwd -P)"
+cd "$(pwd -P)/stage0"
 
 echo "Building stage0..."
 time docker build -t ccondit/linuxfromscratch-stage0 .
@@ -12,5 +12,13 @@ docker run --name=linuxfromscratch-stage0-tmp ccondit/linuxfromscratch-stage0
 
 echo "Exporting filesystem..."
 docker export linuxfromscratch-stage0-tmp | gzip -c > ../linuxfromscratch-stage0.tar.gz
+
+echo "Removing temporary resources..."
+docker stop linuxfromscratch-stage0-tmp
+docker rm linuxfromscratch-stage0-tmp
+docker rmi ccondit/linuxfromscratch-stage0
+
+echo "Loading squashed stage0..."
+docker import ../linuxfromscratch-stage0.tar.gz ccondit/linuxfromscratch-stage0
 
 echo "Stage0 generated."
